eb3e8ec94fc63808aebb6437cbc4013d462bdf01
Sheefa Naaz <sheefanaaz6417@gmail.com>
2024-02-04 12:47:14 +0530

    Update BAND NAME GENERATOR.py

--- 100 DAYS OF CODE (PYTHON)/DAY 1/BAND NAME GENERATOR.py
+++ 100 DAYS OF CODE (PYTHON)/DAY 1/BAND NAME GENERATOR.py
@@ -1,1 +1,10 @@
-hhhhhhhhhhhhhhhhhh
+print("Hello! Welcome to the Band Name Generator")
+
+city = input("What city did you grow up in?\n")
+
+pet = input("What is the name of your pet?\n")
+
+band_name = city + " " + pet
+
+print("Your band name could be: " + band_name)
+
